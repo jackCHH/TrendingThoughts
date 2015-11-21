aeb583693f9ca6931106440c8fdf0631f7251dc6
Jack Hou <jack.hou@live.com>
2015-11-21 16:41:42 -0500

    Modified Tweet Model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,10 +1,11 @@
 class Tweet < ActiveRecord::Base
 	extend FriendlyId
+	require 'indico'
 
 	before_save :downcase_fields
 	friendly_id :handle, use: :slugged
 
-	def get_senti_value(string)
+	def self.get_senti_value(string)
 		(Indico.sentiment_hq(string)*100).round(2)
 	end
 
